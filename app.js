65027f897dc35df6536dc0bebb0607f9a55e17ac
Tony Yip <tony@opensource.hk>
2016-04-20 16:19:20 +0800

    Add npm script

--- app.js
+++ app.js
@@ -11,8 +11,5 @@ app.get('/', function (request, response) {
 	response.render('index');
 });
 
-app.get('/tocus', function (request, response) {
-	response.render('tocus');
-});
 
 app.listen(3000, '127.0.1.4');
