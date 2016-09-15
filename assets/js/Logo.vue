8f314308682cbf5e814a124709f557cc1390fa1b
Tony Yip <tony@opensource.hk>
2016-09-16 01:10:19 +0800

    Use vue file to replace plain text template

---
+++ assets/js/Logo.vue
@@ -0,0 +1,22 @@
+<template>
+    <img
+            src="https://cdn.sitconhk.pro/2016/images/logo/sitcon-x-hk-color.png"
+            :style="{height: h, width: 'auto'}"
+    >
+</template>
+
+<script>
+    export default {
+        data() {
+            const height = parseInt(this.height);
+            return {
+                h: height < 0 ? 'auto' : `${height}px`
+            }
+        },
+        props: {
+            height: {
+                default: 140
+            }
+        }
+    }
+</script>
--- assets/js/app.js
+++ assets/js/app.js
@@ -3,7 +3,7 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import routing from './routing';
-import Logo from './Logo';
+import Logo from './Logo.vue';
 
 Vue.component('logo', Logo);
 Vue.use(VueRouter);
@@ -17,7 +17,7 @@ routing(router);
 router.start({}, '#app');
 
 $('html').niceScroll();
-$(document.getElementsByClassName('button-collapse')).sideNav();
+$(Array.prototype.slice.call(document.getElementsByClassName('button-collapse'))).sideNav();
 const side = document.getElementById('side-nav');
 $(side).delegate('li a', 'click', function () {
   setTimeout(function () {
