0a21e7b9a8eb9d20bb895017e227ec187130f817
Tony Yip <tony@opensource.hk>
2016-09-16 01:12:51 +0800

    Improve home page to give more guide

--- assets/js/pages/home.vue
+++ assets/js/pages/home.vue
@@ -1,20 +1,60 @@
 <template>
     <div class="row">
         <div class="col s12 m12">
-            <div class="card">
+            <div class="card card-see-thought">
+                <div class="card-content">
+
+                </div>
                 <div class="card-image">
-                    <logo></logo>
+                    <logo height="140"></logo>
+                    <div class="secondary-text bold-text center-align">SITCON x HK 2016</div>
                 </div>
                 <div class="card-content">
-                    <h2 class="card-title secondary-text bold-text center-align">SITCON x HK</h2>
                     <div class="center-align">
-                        <p>A conference and community for Student</p>
-                        <hr>
-                        <p>Date: 22 Oct 2016</p>
-                        <p>Venue: City University of Hong Kong Academic 1</p>
+                        <p class="text-strong">A conference and community for Student</p>
+                        <p class="secondary-text text-call">22 Oct 2016 (Sat)</p>
+                        <p class="secondary-text text-call">City University of Hong Kong Academic 1</p>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+    <div class="row">
+        <div class="col s6 m4">
+            <a v-link="{ path: './intro'}">
+                <div class="card">
+                    <div class="card-content">
+                        <div class="card-title center-align">About Us</div>
+                    </div>
+                </div>
+            </a>
+        </div>
+        <div class="col s6 m4">
+            <a v-link="{ path: './cfp' }">
+                <div class="card">
+                    <div class="card-content">
+                        <div class="card-title center-align">Want to Talk?</div>
+                    </div>
+                </div>
+            </a>
+        </div>
+        <div class="col s6 m4">
+            <a v-link="{ path: './sponsor' }">
+                <div class="card">
+                    <div class="card-content">
+                        <div class="card-title center-align">Sponsor Us</div>
+                    </div>
+                </div>
+            </a>
+        </div>
+    </div>
+    <intro></intro>
 </template>
+
+<script>
+    export default {
+        components: {
+            'intro': require('./intro.vue')
+        }
+    }
+</script>
