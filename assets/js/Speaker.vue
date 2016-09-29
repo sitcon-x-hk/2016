d64796e1867f9ba59c152d4d15f7f41b04c47e2a
Tony Yip <tony@opensource.hk>
2016-09-30 01:58:15 +0800

    Add Speaker Introduce element

---
+++ assets/js/Speaker.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="col s12 m12">
+    <div class="card">
+      <div class="card-image">
+        <img :src="image">
+      </div>
+      <div class="card-content">
+        <div class="card-title">{{ name }}</div>
+        <ul>
+          <li>Community: {{ community }}</li>
+        </ul>
+        <p>{{ introduce }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      image: {
+        type: String,
+        required: true,
+        twoWays: false
+      },
+      name: {
+        type: String,
+        required: true,
+        twoWays: false
+      },
+      community: {
+        type: String,
+        required: true,
+        twoWays: false
+      },
+      communityLogo: {
+        type: String,
+        twoWays: false
+      },
+      introduce: {
+        type: String,
+        required: true,
+        twoWays: false
+      }
+    }
+  }
+</script>
